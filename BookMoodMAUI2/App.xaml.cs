0992631a77c4b033aab815324f61a9b2a86f5e5f
Анна Белоусова <belousovaao@mail.ru>
2025-11-06 07:36:49 +0300

    deleted appshell

--- BookMoodMAUI2/App.xaml.cs
+++ BookMoodMAUI2/App.xaml.cs
@@ -4,14 +4,17 @@ namespace BookMoodMAUI2
 {
     public partial class App : Application
     {
-        public App()
+        private readonly IServiceProvider _serviceProvider;
+        public App(IServiceProvider serviceProvider)
         {
             InitializeComponent();
-        }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            _serviceProvider = serviceProvider;
+
+            MainPage = _serviceProvider.GetRequiredService<Pages.MainPage>();
+
+            Application.Current.UserAppTheme = AppTheme.Dark;
+            _serviceProvider = serviceProvider;
         }
     }
 }
